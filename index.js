93f77364e91f98a39eeb4331c661ca5dc2cf7e97
Kayk Mascarenhas <ksm.kayk@outlook.com>
2020-05-17 11:55:12 -0300

    perguntar prefixo

--- index.js
+++ index.js
@@ -4,9 +4,18 @@ function start() {
   const content = {}
 
   content.searchTerm = askAndReturnSearchTerm()
+  content.prefix = askAndReturnPrefix()
 
   function askAndReturnSearchTerm() {
     return readline.question("Type a Wikipedia search term: ")
+  }
+
+  function askAndReturnPrefix() {
+    const prefixes = ["Who is", "What is", "The history of"]
+    const selectedPrefixIndex = readline.keyInSelect(prefixes, "Choose one option: ")
+    const selectedPrefixText = prefixes[selectedPrefixIndex]
+
+    return selectedPrefixText
   }
 
   console.log(content)
