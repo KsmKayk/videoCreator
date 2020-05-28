12dc6089ce3f72265d01cb2dceaa64517d510cce
kayk mascarenhas <ksm.kayk@outlook.com>
2020-05-28 17:40:48 -0300

    MG Error

--- index.js
+++ index.js
@@ -7,12 +7,12 @@ const robots = {
 
 async function start() {
 
-  robots.userInput();
-  await robots.text();
+  //robots.userInput();
+  //await robots.text();
   await robots.image()
 
-  const content = robots.state.load()
-  console.dir(content, { depth: null})
+  //const content = robots.state.load()
+  //console.dir(content, { depth: null})
 }
 
 start();
--- robots/image.js
+++ robots/image.js
@@ -1,5 +1,6 @@
 const imgUrlBlackList = require('../content/blacklistimg.json').imageUrlBlackList
 const imageDownloader = require("image-downloader")
+const gm = require("gm").subClass({imageMagick: true})
 const state =  require("./state")
 const google = require("googleapis").google
 const customSearch = google.customsearch("v1")
@@ -9,11 +10,13 @@ const googleSearchCredentials = require("../credentials/googleSearch.json")
 async function robot() {
   const content = state.load()
 
-  await fetchImagesOfAllSentences(content)
+  //await fetchImagesOfAllSentences(content)
 
-  await downloadAllImages(content) 
+  //await downloadAllImages(content)
+  
+  await convertAllImages(content)
 
-  state.save(content)
+  //state.save(content)
 
   async function fetchImagesOfAllSentences(content) {
     for(const sentence of content.sentences) {
@@ -73,6 +76,51 @@ async function robot() {
       dest: `./content/${fileName}`
     })
   }
+
+  async function convertAllImages(content) {
+    for (let sentenceIndex = 0; sentenceIndex < content.sentences.length; sentenceIndex++) {
+      await convertImage(sentenceIndex)
+    }
+  }
+  async function convertImage(sentenceIndex) {
+    return new Promise((resolve, reject) => {
+      const inputFile = `./content/${sentenceIndex}-original.png[0]`
+      const outputFile = `./content/${sentenceIndex}-converted.png`
+      const width = 1920
+      const height = 1080
+
+      gm(`content/${sentenceIndex}-original.png[0]`)
+        .in()
+        .out('(')
+          .out('-clone')
+          .out('0')
+          .out('-background', 'white')
+          .out('-blur', '0x9')
+          .out('-resize', `${width}x${height}^`)
+        .out(')')
+        .out('(')
+          .out('-clone')
+          .out('0')
+          .out('-background', 'white')
+          .out('-resize', `${width}x${height}`)
+        .out(')')
+        .out('-delete', '0')
+        .out('-gravity', 'center')
+        .out('-compose', 'over')
+        .out('-composite')
+        .out('-extent', `${width}x${height}`)
+        .write(`content/${sentenceIndex}-converted.png`, (error) => {
+          if (error) {
+            return reject(error)
+          }
+
+          console.log(`> Image converted: ${inputFile}`)
+          resolve()
+        })
+
+    })
+  }
+
 }
 
 module.exports = robot
